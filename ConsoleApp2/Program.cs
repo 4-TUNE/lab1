750be1875d4ef178dba923baf36a7053064ea953
4-TUNE <61144719+4-TUNE@users.noreply.github.com>
2020-05-11 13:28:54 +0300

    1111

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static public void Example(double a, double b)
+    {
+
+      
+        double c = Math.Sqrt(a * a + b * b);
+        Console.WriteLine("Гiпотенуза = " + c); 
+        double p = a + b + c;
+        Console.WriteLine("Периметр = " + p);
+    }
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введiть перше число"); 
+        string S1 = Console.ReadLine();
+        double a = double.Parse(S1); 
+        Console.WriteLine("Введiть друге число"); 
+        string S2 = Console.ReadLine();
+        double b = double.Parse(S2); 
+        Example(a, b);
+        Console.ReadLine();
+    }
+}
